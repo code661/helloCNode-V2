577543d5621b66806e3b87fb79132b158c2f72cc
Ryan <lucky.tanduzhi@gmail.com>
2018-10-20 19:27:15 +0800

    给内容区加最小高度

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <slot name="header">
         <Header></Header>
       </slot>
-      <Content :style="{padding: '0 50px', marginTop: '20px'}">
+      <Content :style="{padding: '0 50px', marginTop: '20px', minHeight: 'calc(100vh - 153px)'}">
         <div class="main">
           <router-view></router-view>
         </div>
@@ -24,17 +24,19 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+.layout {
+  background: #f5f7f9;
+  position: relative;
+  overflow: hidden;
 
-.layout
-  background #f5f7f9
-  position: relative
-  overflow: hidden
-  .main
-    min-width 800px
-  .layout-footer-center
-    text-align: center
+  .main {
+    min-width: 800px;
+  }
 
-
+  .layout-footer-center {
+    text-align: center;
+  }
+}
 </style>
 
 
